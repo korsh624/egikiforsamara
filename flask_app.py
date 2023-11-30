30d645fd2a0a36f9b8217081ebf5bed9d0de1aa6
korsh624 <korsh624@gmail.com>
2023-11-30 08:22:48 +0300

    add inner pages and test

--- flask_app.py
+++ flask_app.py
@@ -4,5 +4,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/plice")
+def plice():
+    return render_template('item.html')
+
+
 if __name__=="__main__":
     app.run(debug=True)
